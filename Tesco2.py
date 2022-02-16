593d0c5e7e2eec8185610b1296109086641567e1
Haydn <h.t.beard@hotmail.co.uk>
2022-02-16 10:24:35 +0000

    hello

---
+++ Tesco2.py
@@ -0,0 +1,10 @@
+product=input("Product Bought:")
+quantity=int(input("How much do you need?"))
+price=int(input("Enter how much it costs"))
+bill=quantity*price
+print("------------------")
+print("Your invoice is")
+print("1.Product:",product)
+print("2.Quantity:",quantity)
+print("3.Price:",price)
+print("Your Bill is:",bill)
